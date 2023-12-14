1b51cfdd94b5ffd75e0ebc506e35684aa08e0226
diasbouk <boukdsaid@gmail.com>
2023-12-14 17:19:38 +0100

    Task 0 : STDERR check

--- monty.c
+++ monty.c
@@ -7,50 +7,17 @@ void push_num(stack_t **stack, unsigned int line_number)
 	(*stack)->n = line_number;
 }
 
+
+
 int main(int argc, char **argv)
 {
-	FILE *monty_file;
-	char *buffer = NULL, *command;
-	size_t buffer_size = 0;
-	stack_t *head;
-	unsigned int num;
-
-
-		head = malloc(sizeof(stack_t));
-		head->prev = NULL;
+	(void)argv;
 
 		if (argc != 2)
 		{
-			write(STDERR_FILENO, "USAGE: monty file,", 18);
+			write(STDERR_FILENO, "USAGE: monty file", 17);
 			exit(EXIT_FAILURE);
 		}
-
-
-		monty_file = fopen(*(argv + 1), "r");
-		if (monty_file == NULL)
-		{
-			printf("Error: Can't open file %s", *(argv + 1));
-			exit(EXIT_FAILURE);
-		}
-
-
-		
-		if (feof(monty_file))
-			printf("L<line_number>: unknown instruction %s", *(argv + 1));
-
-		while (fgets(buffer, buffer_size, monty_file) != NULL)
-		{
-			command = strtok(buffer, DELIM);
-			if (strcmp(command, "push"))
-			{
-				num = atoi(strtok(NULL, DELIM));
-				if (isdigit(num))
-					push_num(&head, num);
-				printf("%d\n", head->n);
-			}
-		}	
-
-		free(head);
 
 		return (0);
 }
