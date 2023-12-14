04a7ed8977ec2a45c589d2b1321c9eb4f8100020
diasbouk <boukdsaid@gmail.com>
2023-12-14 17:00:03 +0100

    Task 0 : Code check

--- monty.c
+++ monty.c
@@ -1,17 +1,7 @@
 #include "monty.h"
 
 
-void check_instuct(instruction_t inst, char *opcode)
-{
-	char *part_1, *part_2;
-	if (!opcode)
-		write(STDERR_FILENO, "Invalide instruction", 20);
-	part_1 = strtok(opcode, DELIM);
-	part_2 = strtok(NULL, DELIM);
-	if (strcmp(part_1, "push") == 0)
-		if (isdigit(atoi(part_2)))
-			atoi(part_2);
-}
+
 void push_num(stack_t **stack, unsigned int line_number)
 {
 	(*stack)->n = line_number;
@@ -20,13 +10,14 @@ void push_num(stack_t **stack, unsigned int line_number)
 int main(int argc, char **argv)
 {
 	FILE *monty_file;
-	char *buffer;
-	size_t buffer_size;
-	int num_chars;
-	instruction_t push, pall;
-	stack_t **stack;
+	char *buffer = NULL, *command;
+	size_t buffer_size = 0;
+	stack_t *head;
 	unsigned int num;
 
+
+		head = malloc(sizeof(stack_t));
+		head->prev = NULL;
 
 		if (argc != 2)
 		{
@@ -43,15 +34,23 @@ int main(int argc, char **argv)
 		}
 
 
-		num_chars = getline(&buffer, &buffer_size, monty_file);
-		if (num_chars == -1)
-			printf("L<line_number>: unknown instruction %s", buffer);
+		
+		if (feof(monty_file))
+			printf("L<line_number>: unknown instruction %s", *(argv + 1));
 
-		if (strcmp(strtok(buffer, DELIM), "push") == 0)
+		while (fgets(buffer, buffer_size, monty_file) != NULL)
 		{
-			num = atoi(strtok(NULL, DELIM));
-			if (isdigit(num))
-				push_num(stack, num);
-			printf("%d\n", (*stack)->n);
-		}
+			command = strtok(buffer, DELIM);
+			if (strcmp(command, "push"))
+			{
+				num = atoi(strtok(NULL, DELIM));
+				if (isdigit(num))
+					push_num(&head, num);
+				printf("%d\n", head->n);
+			}
+		}	
+
+		free(head);
+
+		return (0);
 }
